e2619bb72ff92f4ab39b54fe626b41102d07a93c
koa2019 <no-reply>
2019-10-24 17:43:59 -0700

    rendered compontent> Nav to app.js & added custom styles to app.css for Nav

---
+++ src/components/Nav/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Nav() {
+    return (
+        <nav className="nav nav-pills flex-column flex-sm-row nav-justified">
+            <a className="nav-item flex-sm-fill text-sm-center nav-link" href="/">Clicky Game</a>
+                <a className="nav-item flex-sm-fill text-sm-center nav-link" href="/"><span>Click an Image to Start!</span></a>
+                <a className="nav-item flex-sm-fill text-sm-center nav-link" href="/">Score: 0 | Top Score: 9</a>
+        </nav>
+    );
+}
+
+export default Nav;
