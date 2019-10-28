2e6497b19384cb86efdf82ad035824d0a5ab9eab
koa2019 <no-reply>
2019-10-28 15:09:19 -0700

    cleaned up the code. added numCorrect to Navbar. added resetGame()

--- src/components/BodyContainer/BodyContainer.js
+++ src/components/BodyContainer/BodyContainer.js
@@ -9,84 +9,104 @@ import Modal from "../Modal";
 import Images from '../../components/images.json';
 import "./style.css";
 
-// const maxScore = 75;
-const maxCorrect = 15;
-let numCorrect = 0;
-let selectedImages = [];
+const maxScore = 150;
+const maxCorrect = 3;
 
 class BodyContainer extends Component {
 
     state = {
-        status: "",
+        status: 'go',
+        numCorrect: 0,
         currentScore: 0,
         topScore: 0,
         points: 1,
-        allImages: Images
+        allImages: Images,
+        message: ''
     };
 
+    resetGame = () => {
+
+        // clone state obj
+        const newState = { ...this.state };
+        newState.currentScore = 0;
+        newState.status = 'go';
+        newState.numCorrect = 0;
+        this.getRandomPoints();
+        this.setState(newState)
+        console.log(newState)
+    }
+
     getRandomPoints = () => {
-        const num = Math.floor(Math.random() * 5) + 1;
+        const num = Math.floor(Math.random() * 10) + 1;
         this.setState({
             points: num
         });
     }
 
+    renderMessage = str => {
+        switch (str) {
+            case 'win':
+                this.setState({ message: 'WINNER! You Guessed 15 out of 15 Correct!' })
+                this.setState({ status: 'stop' })
+                this.resetGame()
+                break;
+            case 'stop':
+                this.setState({ message: 'GAME OVER. You Guessed Wrong' })
+                this.setState({ status: 'stop' })
+                this.resetGame()
+                break;
+            default:
+                this.setState({ message: 'Hello' })
+                this.setState({ status: 'go' })
+        }
+    }
+
+    // function to render images in random order
     renderCards = Images => {
         this.state.allImages.shuffle();
     }
 
-    renderMessage = str => {
-        switch (str) {
-            case 'win':
-                alert('WINNER! You Guessed 15 out of 15 Correct!');
-                break;
-            case 'lost':
-                alert('Sorry You Guessed Wrong');
-                break;
-            default:
-                alert('Hello');
-        }
-    }
-    validateSelection = selectedId => {
+    // function checks if the img user clicks on has already been clicked on 
+    // by searching for it in the clickedImgs array
+    validateSelection = currentImg => {
 
+        let clickedImgs = [];
         // for (character of selectedImages) {
         //     if(selectedId == i) {
-        selectedImages.indexOf(selectedId, 0)
-        if (selectedId) {
-            this.setState({
-                status: 'end'
-            })
+        clickedImgs.filter(currentImg)
+        if (currentImg) {
+            this.renderMessage('stop')
         }
         else {
-            selectedImages.push(selectedId)
-            this.setState({
-                status: 'play'
-            })
+            clickedImgs.push(currentImg)
         }
     }
 
-    handleCardClick = event => {
 
+    // eventListener function to handle card user selects
+    handleCardClick = event => {
         event.preventDefault();
+
         // Get the id of the clicked card
         // const selectedId = event.target.attributes.getNamedItem('data-id');
         const selectedId = event.target.attributes.value;
         console.log(selectedId)
 
+        // clone state obj. Easier way to update multiple key prop at once
+        const newState = { ...this.state };
+
         // call this function
-        this.validateSelection(selectedId);
+        // this.validateSelection(selectedId);
+        if (newState.status === 'go') {
 
-        if (this.state.status === 'play') {
-
-            numCorrect += 1;
-
-            // clone state obj
-            const newState = { ...this.state };
+            newState.numCorrect += 1;
 
             // set score to the sum of current score & points
+            // this.getCurrentScore()
             newState.currentScore += this.state.points;
 
             // conditional to find top score
+            // this.getTopScore()
             if (newState.currentScore > newState.topScore) {
                 newState.topScore = newState.currentScore
             }
@@ -94,15 +114,15 @@ class BodyContainer extends Component {
             // set Images isClicked property to true
             // newState.allImages.isClicked = true;
 
-           // Replace our component's state with newState
+            // Replace our component's state with newState
             this.setState(newState);
             // load new random points value to card
             this.getRandomPoints();
         }
 
-        // conditional to end game if player picked all 15 cards only once
-        if (numCorrect === maxCorrect) {
-            this.setState({ status: 'end' })
+        // conditional to stop game if goer picked all 15 cards only once
+        if (newState.numCorrect === maxCorrect) {
+            console.log('WINNER! You Guessed 15 out of 15 Correct!')
             this.renderMessage('win')
         }
     }
@@ -110,7 +130,7 @@ class BodyContainer extends Component {
     render() {
         return (
             <div>
-                <Nav currentScore={this.state.currentScore} topScore={this.state.topScore} />
+                <Nav currentScore={this.state.currentScore} topScore={this.state.topScore} correct={this.state.numCorrect} />
                 <Header />
                 <Container>
                     <Row>
@@ -131,7 +151,9 @@ class BodyContainer extends Component {
 
                     </Row>
                 </Container>
-                <Modal />
+                <Modal>
+                    {this.state.message}
+                </Modal>
             </div>
         );
     }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,7 +4,7 @@ function Nav(props) {
     return (
         <nav className="nav navbar nav-pills flex-column flex-sm-row nav-justified">
             <a className="nav-item flex-sm-fill text-sm-center nav-link" href="/">Clicky Game</a>
-            <a className="nav-item flex-sm-fill text-sm-center nav-link" href="/"><span>Click an Image to Start!</span></a>
+            <a className="nav-item flex-sm-fill text-sm-center nav-link" href="/"># Correct Guesses: {props.correct}</a>
             <a className="nav-item flex-sm-fill text-sm-center nav-link" href="/">Score: {props.currentScore} | Top Score: {props.topScore}</a>
         </nav>
     );
