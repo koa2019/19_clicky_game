61720634eebfab25e36ec6ff21d3dbabf297be68
koa2019 <no-reply>
2020-05-17 16:44:01 -0700

    adjusted nav spacing & text alignment

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,8 +4,8 @@ function Header(props) {
 
     return (
         <header className='jumbotron'>
-            <h3 className='mt-5'>Test your memory.</h3>
-            <h4>Click each image only once to accumulate the most points</h4>
+            <h4 className='mt-5'>Test your memory.</h4>
+            <h4>Click each image only once to accumulate the most points.</h4>
             <h2>Click an image twice. GAME OVER!</h2>
         </header>
     );
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,9 +3,9 @@ import React from "react";
 function Nav(props) {
     return (
         <nav className="nav navbar nav-pills flex-column flex-sm-row nav-justified fixed-top">
-            <a className="nav-item flex-sm-fill text-sm-center nav-link" href="/">Clicky Game</a>
-            <a className="nav-item flex-sm-fill text-sm-center nav-link" href="/"># Correct Guesses: {props.correct}</a>
-            <a className="nav-item flex-sm-fill text-sm-center nav-link" href="/">Score: {props.currentScore} | Top Score: {props.topScore}</a>
+            <a className="nav-item flex-sm-fill text-lg-center nav-link" href="/">Clicky Game</a>
+            <a className="nav-item flex-sm-fill text-lg-center nav-link" href="/"># Correct Guesses: {props.correct}</a>
+            <a className="nav-item flex-sm-fill text-lg-center nav-link" href="/">Score: {props.currentScore} | Top Score: {props.topScore}</a>
         </nav>
     );
 }
