7a14c6f228942de34deecea7e060f170f866a87c
koa2019 <no-reply>
2020-05-17 15:30:22 -0700

    removed br tag in header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,9 +4,7 @@ function Header(props) {
 
     return (
         <header className='jumbotron'>
-            {/* <h1>Clicky Game</h1>          */}
-            <h3 className='mt-4'>Test your memory. Click on an image once to earn points and continure playing. 
-            <br> Good Luck!</br></h3>
+            <h3 className='mt-4'>Test your memory. Click on an image once to earn points and continure playing. Good Luck!</h3>
         </header>
     );
 }
