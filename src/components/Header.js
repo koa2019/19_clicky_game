13cd7273186d8ca57f7091ea5ff5cc30493612c4
koa2019 <no-reply>
2020-05-17 15:27:57 -0700

    removed redunant texts in header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ function Header(props) {
 
     return (
         <header className='jumbotron'>
-            <h1>Clicky Game</h1>         
+            {/* <h1>Clicky Game</h1>          */}
             <h3 className='mt-4'>Test your memory. Click on an image once to earn points and continure playing. 
             <br> Good Luck!</br></h3>
         </header>
